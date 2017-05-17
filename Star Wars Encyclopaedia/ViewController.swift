bb1f0db73c60360bcac198a029fd16da0687e205
Robert Tichy <robtichy@AOL.com>
2017-05-17 11:08:25 -0400

    Initial Commit

---
+++ Star Wars Encyclopaedia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Star Wars Encyclopaedia
+//
+//  Created by Robert G Tichy on 5/17/17.
+//  Copyright © 2017 Robert G Tichy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
